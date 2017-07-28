4053dc6e0ecbc8741f98658e48fc389f7af727f0
Fabio Berton <fabio.berton@ossystems.com.br>
2017-07-28 15:50:12 -0300

    ARM: raspberrypi: Change DTB names to work with linux-raspberrypi
    The device tree names in this file are to with use mainline kernel, but
    UpdateHub is currently using kernel source tree from Raspberry Pi
    Foundation and the device tree name is different from mainline:
    
      For 3 Compute Module -> bcm2710-rpi-cm3.dtb
      For 3 Model B+       -> bcm2710-rpi-3-b-plus.dtb
      For 3 Model B        -> bcm2710-rpi-3-b.dtb
      For 2 Model B        -> bcm2709-rpi-2-b.dtb
      For Zero             -> bcm2708-rpi-b.dtb
      For Model B          -> bcm2708-rpi-b.dtb
      For Model B+         -> bcm2708-rpi-b-plus.dtb
    
    Signed-off-by: Fabio Berton <fabio.berton@ossystems.com.br>
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- board/raspberrypi/rpi/rpi.c
+++ board/raspberrypi/rpi/rpi.c
@@ -112,7 +112,7 @@ static const struct rpi_model rpi_models_new_scheme[] = {
 	},
 	[0x4] = {
 		"2 Model B",
-		DTB_DIR "bcm2836-rpi-2-b.dtb",
+		DTB_DIR "bcm2709-rpi-2-b.dtb",
 		true,
 	},
 	[0x6] = {
@@ -122,17 +122,17 @@ static const struct rpi_model rpi_models_new_scheme[] = {
 	},
 	[0x8] = {
 		"3 Model B",
-		DTB_DIR "bcm2837-rpi-3-b.dtb",
+		DTB_DIR "bcm2710-rpi-3-b.dtb",
 		true,
 	},
 	[0x9] = {
 		"Zero",
-		DTB_DIR "bcm2835-rpi-zero.dtb",
+		DTB_DIR "bcm2708-rpi-b.dtb",
 		false,
 	},
 	[0xA] = {
 		"Compute Module 3",
-		DTB_DIR "bcm2837-rpi-cm3.dtb",
+		DTB_DIR "bcm2710-rpi-cm3.dtb",
 		false,
 	},
 	[0xC] = {
@@ -142,7 +142,7 @@ static const struct rpi_model rpi_models_new_scheme[] = {
 	},
 	[0xD] = {
 		"3 Model B+",
-		DTB_DIR "bcm2837-rpi-3-b-plus.dtb",
+		DTB_DIR "bcm2710-rpi-3-b-plus.dtb",
 		true,
 	},
 };
@@ -150,27 +150,27 @@ static const struct rpi_model rpi_models_new_scheme[] = {
 static const struct rpi_model rpi_models_old_scheme[] = {
 	[0x2] = {
 		"Model B",
-		DTB_DIR "bcm2835-rpi-b.dtb",
+		DTB_DIR "bcm2708-rpi-b.dtb",
 		true,
 	},
 	[0x3] = {
 		"Model B",
-		DTB_DIR "bcm2835-rpi-b.dtb",
+		DTB_DIR "bcm2708-rpi-b.dtb",
 		true,
 	},
 	[0x4] = {
 		"Model B rev2",
-		DTB_DIR "bcm2835-rpi-b-rev2.dtb",
+		DTB_DIR "bcm2708-rpi-b.dtb",
 		true,
 	},
 	[0x5] = {
 		"Model B rev2",
-		DTB_DIR "bcm2835-rpi-b-rev2.dtb",
+		DTB_DIR "bcm2708-rpi-b.dtb",
 		true,
 	},
 	[0x6] = {
 		"Model B rev2",
-		DTB_DIR "bcm2835-rpi-b-rev2.dtb",
+		DTB_DIR "bcm2708-rpi-b.dtb",
 		true,
 	},
 	[0x7] = {
@@ -190,22 +190,22 @@ static const struct rpi_model rpi_models_old_scheme[] = {
 	},
 	[0xd] = {
 		"Model B rev2",
-		DTB_DIR "bcm2835-rpi-b-rev2.dtb",
+		DTB_DIR "bcm2708-rpi-b.dtb",
 		true,
 	},
 	[0xe] = {
 		"Model B rev2",
-		DTB_DIR "bcm2835-rpi-b-rev2.dtb",
+		DTB_DIR "bcm2708-rpi-b.dtb",
 		true,
 	},
 	[0xf] = {
 		"Model B rev2",
-		DTB_DIR "bcm2835-rpi-b-rev2.dtb",
+		DTB_DIR "bcm2708-rpi-b.dtb",
 		true,
 	},
 	[0x10] = {
 		"Model B+",
-		DTB_DIR "bcm2835-rpi-b-plus.dtb",
+		DTB_DIR "bcm2708-rpi-b-plus.dtb",
 		true,
 	},
 	[0x11] = {
@@ -220,7 +220,7 @@ static const struct rpi_model rpi_models_old_scheme[] = {
 	},
 	[0x13] = {
 		"Model B+",
-		DTB_DIR "bcm2835-rpi-b-plus.dtb",
+		DTB_DIR "bcm2708-rpi-b-plus.dtb",
 		true,
 	},
 	[0x14] = {
